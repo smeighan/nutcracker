5f57c5ab24961a2c64c0922db57eb940a5cc5567
sean meighan <sean.meighan@oracle.com>
2012-08-16 09:47:05 -0600

    delete some target files

--- effects/copy_effect.sql
+++
@@ -1,2 +0,0 @@
-insert into effects_dtl select 'Y','color_wash',param_name,param_prompt, param_desc,param_range,now(),now(),sequence 
-	   from effects_dtl where effect_class='garlands'
--- effects/sql1.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM effects_user_hdr a, effects_user_dtl b, effects_dtl c
-where a.username=b.username
-and a.effect_name=b.effect_name
-and a.effect_class=c.effect_class
-and a.effect_class='butterfly'
- and a.username='f' 
-order by a.username,a.effect_name,c.sequence
