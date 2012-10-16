5429bf6486711b9950c71e8927599431a2c61316
Kurt Gustafson <kgustafson2@gmail.com>
2012-10-16 08:38:10 -0400

    Project v1.10
    Major upgrades to project and project_filer to use effect_id instead of
    username/effect pair in project_detail.  NOTE:  This requires a rebuild
    of the project data, since there is an added field 'effect_id' in
    project_detail.  Without this change, these updates to project will not
    work.
    ADDED FUNCTION : Now a user can click on the thumbnail of an effect and
    edit that effect directly from project.  The changes to the parameters
    of the effect are saved (if the user clicks submit) and they are
    returned to the phrase edit window of project after the submit or
    cancel.

--- sql/make_project_tables.sql
+++ sql/make_project_tables.sql
@@ -66,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `project_dtl` (
   `effect_name` varchar(25) NOT NULL,
   `project_id` int(11) NOT NULL,
   `check_sum` varchar(50) DEFAULT NULL,
+  `effect_id` int(11) NOT NULL,
   PRIMARY KEY (`project_detail_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
