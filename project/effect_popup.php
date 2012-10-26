9e6516e7f1a8e82fd3a50f84ceb1e8a7a3e90cc9
Kurt Gustafson <kgustafson2@gmail.com>
2012-10-26 10:08:44 -0400

    Project v1.10.2
    Modded effect popup to include the color effects and color picker like
    the stand-alone effects functionality.

--- project/effect_popup.php
+++ project/effect_popup.php
@@ -11,6 +11,7 @@ $effect_class=$row['effect_class'];
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <header><title>Get Effect Details</title>
+<script type="text/javascript" src="../effects/jscolor.js"></script>
 <link href="../css/ncFormDefault.css" rel="stylesheet" type="text/css" />
 </header>
 <body>
@@ -37,7 +38,17 @@ function projEffectEdit($effect_id, $project_id) {
 				$trStr='<tr>';
 			else
 				$trStr='<tr class="alt">';
-			$fieldstr=$trStr.'<td><input type="text" name="'.$param_name.'" value="'.$param_value.'"></td><td>'.$param_prompt.'</td><td>'.$param_desc.'</td><td>'.$param_range.'</td></tr>'."\n";
+			//$mystring = $effect_details[$i]['param_name'];
+			$findme="color";
+			$pos = strpos($param_name, $findme);
+			if ($pos === false)
+			{
+				$classStr=" class=\"input\" ";
+			}
+			else {
+				$classStr=" class=\"color {hash:true} {pickerMode:'HSV'}\" ";
+			}
+			$fieldstr=$trStr.'<td><input type="text" name="'.$param_name.'" '.$classStr.' value="'.$param_value.'"></td><td>'.$param_prompt.'</td><td>'.$param_desc.'</td><td>'.$param_range.'</td></tr>'."\n";
 			echo $fieldstr;
 			$cnt++;
 		} 
