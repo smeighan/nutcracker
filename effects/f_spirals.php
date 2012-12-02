35336849c8840eb6233575e1f31dc124439c9531
sean meighan <sean.meighan@oracle.com>
2012-12-02 10:54:05 -0700

    Added no compression apache setup to f_spirals. see if this helps teh caching issue while writing to a file.
    dec 2 export of data

--- effects/f_spirals.php
+++ effects/f_spirals.php
@@ -22,6 +22,14 @@ function f_spirals($get)
 	extract ($get);
 	set_time_limit(0);
 	ini_set("memory_limit","1024M");
+	
+    @apache_setenv('no-gzip', 1);
+    @ini_set('zlib.output_compression', 0);
+    @ini_set('implicit_flush', 1);
+ //   for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
+ //   ob_implicit_flush(1);
+
+
 	require_once("../effects/read_file.php");
 	//
 	//
