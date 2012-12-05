3d05e211abe3cb90941a3d84ecacba12d2294df2
sean meighan <sean.meighan@oracle.com>
2012-12-04 17:20:32 -0700

    new setup containing initial setup for apache

---
+++ conf/setup.php
@@ -0,0 +1,8 @@
+<?php
+	set_time_limit(0);
+	ini_set("memory_limit","1024M");
+	
+    @apache_setenv('no-gzip', 1);
+    @ini_set('zlib.output_compression', 0);
+    @ini_set('implicit_flush', 1);
+?>
